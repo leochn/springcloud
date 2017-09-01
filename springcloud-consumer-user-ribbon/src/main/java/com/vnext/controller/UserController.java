db32da5bf0a8d723546bcfeba65367b797e56eb5
leo <appchn@163.com>
2017-09-02 07:21:46 +0800

    add

---
+++ springcloud-consumer-user-ribbon/src/main/java/com/vnext/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.vnext.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.vnext.core.Result;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${user.userServicePath}")
+	private String userServicePath;
+
+	@GetMapping("/consumer/sysUsers")
+	public Result findById() {
+		return this.restTemplate.getForObject(this.userServicePath, Result.class);
+	}
+	
+	@GetMapping("/consumer/all")
+	public Result getAll() {
+		// userServicePath: http://localhost:8087/api/sysUsers
+		// SH-RAD02-V17.shwpg.com:springcloud-provide-user:8087
+		return this.restTemplate.getForObject("http://springcloud-provide-user/api/sysUsers", Result.class);
+	}
+
+}
