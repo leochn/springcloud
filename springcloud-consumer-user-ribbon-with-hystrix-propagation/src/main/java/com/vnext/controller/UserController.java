0f9714f85aa8ed9d94623d4b1a1e5ad8754f66f4
leochn <appchn@163.com>
2017-09-21 08:14:07 +0800

    add module

---
+++ springcloud-consumer-user-ribbon-with-hystrix-propagation/src/main/java/com/vnext/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.vnext.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.vnext.core.Result;
+import com.vnext.core.ResultGenerator;
+import com.vnext.pojo.SysUser;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@GetMapping("/consumer/all")
+	@HystrixCommand(fallbackMethod = "getAllFallback",commandProperties = @HystrixProperty(name = "execution.isolation.strategy", value = "SEMAPHORE"))
+	public Result getAll() {
+		// userServicePath: http://localhost:8087/api/sysUsers
+		// SH-RAD02-V17.shwpg.com:springcloud-provide-user:8087
+		System.out.println("hystrix......into..........");
+		return this.restTemplate.getForObject("http://springcloud-provide-user/api/sysUsers", Result.class);
+	}
+
+	public Result getAllFallback() {
+		SysUser sysUser = new SysUser();
+		sysUser.setId("0L");
+		sysUser.setLoginName("0L");
+		return ResultGenerator.genSuccessResult(1, sysUser);
+	}
+
+}
--- springcloud-gateway-zuul/README.md
+++ springcloud-gateway-zuul/README.md
@@ -1,29 +1,51 @@
 
 1.默认：
-http://10.127.127.1:9005/springcloud-provide-user/api/sysUsers
+```
+spring:
+  application:
+    name: springcloud-gateway-zuul
+
+server:
+  port: 9040
+
+eureka:
+  client:
+    service-url:
+      defaultZone: http://localhost:9001/eureka
+  instance:
+    prefer-ip-address: true
+```
+
+请求url --> http://10.127.127.1:9005/springcloud-provide-user/api/sysUsers
 
 2.配置
+```
 zuul:
   routes:
     springcloud-provide-user: /users/**
+```
 
-http://10.127.127.1:9005/users/api/sysUsers
+请求url --> http://10.127.127.1:9005/users/api/sysUsers
 
 3.配置：path + service-id
+```
 zuul:
   routes:
     userABC:  # userABC 可以随意命名，只要是唯一
       path: /users-path/**
       service-id: springcloud-provide-user
+```
 
-http://10.127.127.1:9005/users-path/api/sysUsers
+请求url --> http://10.127.127.1:9005/users-path/api/sysUsers
 
 4.配置：path + url
+```
 zuul:
   routes:
     userABC:
       path: /users-path/**
       url: http://localhost:8087
+```
 
 http://10.127.127.1:9005/users-path/api/sysUsers
 
